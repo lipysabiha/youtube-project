7b23d871c25855dcdab4855451ff091b7382b43e
unknown <lipysabiha@gamil.com>
2021-08-24 22:30:16 +0600

    youtube-project

---
+++ src/components/Navbar.js
@@ -0,0 +1,114 @@
+import React from "react";
+import styled, {
+    css
+} from "styled-components/macro";
+import {
+    Link
+} from "react-router-dom";
+import {
+    menuData
+} from "./../data/MenuData";
+import {
+    Button
+} from "./Button";
+import Bars from "../images/bars.svg";
+import {
+    FaBars
+} from "react-icons/fa";
+
+const Navbar = () => {
+    return ( <
+        Nav >
+        <
+        Logo to = "/" > ELIXR < /Logo>  <
+        MenuBars / >
+
+        <
+        NavMenu > {
+            menuData.map((item, index) => ( <
+                NavMenuLinks to = {
+                    item.links
+                }
+                key = {
+                    index
+                } > {
+                    item.title
+                } <
+                /NavMenuLinks>
+            ))
+        } <
+        /NavMenu>  <
+        NavBtn >
+        <
+        Button to = "/contact"
+        primary = "true" >
+        Contact Us <
+        /Button>  < /
+        NavBtn > <
+        /Nav>
+    )
+}
+
+export default Navbar;
+
+
+
+const Nav = styled.nav `
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
+    background: red;
+`;
+
+const NavLink = css `
+    color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
+`;
+
+const Logo = styled(Link)
+`
+    ${NavLink};
+    font-style: italic;
+`;
+
+const MenuBars = styled {
+    FaBars
+}
+`
+    display: none;
+`;
+
+const NavMenu = styled.div `
+    color: #fff;
+    display: flex;
+    align-items: center;
+    margin-right: -48px;
+
+    @media screen and (max-width: 768px) {
+          display: none;
+      }
+`;
+
+const NavMenuLinks = styled(Link)
+`
+    ${NavLink}
+`;
+
+const NavBtn = styled.div `
+     display: flex;
+     align-items: center;
+     margin-right: 24px;
+
+     @media screen and (max-width: 768px) {
+          display: none;
+      }
+`;
